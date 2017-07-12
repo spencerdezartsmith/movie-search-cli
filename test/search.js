0589274c04f4267dff9536111dbdccce224bbd74
spencerdezartsmith <spencer.dezart.smith@gmail.com>
2017-07-12 15:20:16 -0700

    Set up two example tests

--- test/search.js
+++ test/search.js
@@ -1,29 +1,29 @@
 const expect = require('chai').expect
-const search = require('../app/search')
-const nock = require('nock')
+const { searchIMDB, getMovieTitles } = require('../app/search')
 
-describe('Movie Search Command Line Tool', () => {
-  describe('searchIMDB', () => {
-    it('can find all the titles for Rocky films', (done) => {
-      nock('http://www.imdb.com')
-        .get('/find')
-        .reply(200, `
-          <div class='findSection'>
-            <table class="findList">
-              <tr> <td class="result_text">Rocky (1976)</td> </tr>
-              <tr> <td class="result_text">Rocky (1981)</td> </tr>
-            </table>
-          </div>
-        `)
-
-      search.searchIMDB('rocky', (err, results) => {
-        console.log('hello friends')
-        done()
-      })
+describe('the easy way', () => {
+  it('can find rocky', (done) => {
+    searchIMDB('rocky', (err, result) => {
+      expect(err).to.be.null
+      expect(result).to.have.length(2)
+      done()
     })
   })
+})
 
-  afterEach(function() {
-   nock.restore()
- })
+describe('unit testing', () => {
+  it('can find the titles for rocky', (done) => {
+    const html = `
+      <div class='findSection'>
+        <table class='findList'>
+          <tr><td class='result_text'>Rocky (1976)</tr></td>
+          <tr><td class='result_text'>Rocky (1981)</tr></td>
+        </table>
+      </div>
+    `
+    const result = getMovieTitles(html)
+    expect(result).to.be.an('array')
+    expect(result).to.have.length(2)
+    done()
+  })
 })
