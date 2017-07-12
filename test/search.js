e7e915213c3dcafed8b654788442e6c20d5aab1c
spencerdezartsmith <spencer.dezart.smith@gmail.com>
2017-07-11 18:15:07 -0700

    Set up test structure

--- test/search.js
+++ test/search.js
@@ -2,11 +2,21 @@ const expect = require('chai').expect
 const search = require('../app/search')
 
 describe('Movie Search Command Line Tool', () => {
-  describe('Searches IMDB for movie via get request', () => {
-    it('returns movie data from IMDB', () => {
-      const nemoHTML = search.getMovieData('findingnemo')
+  describe('#searchIMDB', () => {
+    it('is a function', () => {
+      expect(search.searchIMDB).to.be.a('function')
+    })
+  })
 
-      expect(nemoHTML).to.be.a('string')
+  describe('#getMovieTitles', () => {
+    it('is a function', () => {
+      expect(search.getMovieTitles).to.be.a('function')
+    })
+  })
+
+  describe('#run',() => {
+    it('is a function', () => {
+      expect(search.run).to.be.a('function')
     })
   })
 })
